205ea7fa5884d4628fa8c06faa41574d6c90a9f1
elisabeth-ms <emenende@pa.uc3m.es>
2022-10-06 20:34:29 +0200

    Adding environment objects to the collision checker of different shapes. The shapes of the collision objects depends on the superquadric params.

--- libraries/TeoCheckCollisionsLibrary/TeoCheckCollisionsLibrary.cpp
+++ libraries/TeoCheckCollisionsLibrary/TeoCheckCollisionsLibrary.cpp
@@ -7,16 +7,19 @@ constexpr auto DEFAULT_NUM_LINKS = 1;
 
 namespace roboticslab
 {
-    void TeoCheckCollisionsLibrary::getFixedObjectTransformations(std::vector<std::array<double,7>> & transformations){
-        for(int i=0; i<m_fixedCollisionObjects.size(); i++){
+    void TeoCheckCollisionsLibrary::getFixedObjectTransformations(std::vector<std::array<double, 7>> &transformations)
+    {
+        for (int i = 0; i < m_fixedCollisionObjects.size(); i++)
+        {
             fcl::Quaternionf quat = m_fixedCollisionObjects[i].getQuatRotation();
             fcl::Vector3f translation = m_fixedCollisionObjects[i].getTranslation();
-            std::array<double,7> transformation = {quat.x(), quat.y(), quat.z(), quat.w(), translation[0], translation[1], translation[2]};
+            std::array<double, 7> transformation = {quat.x(), quat.y(), quat.z(), quat.w(), translation[0], translation[1], translation[2]};
             transformations.push_back(transformation);
         }
     }
 
-    void TeoCheckCollisionsLibrary::configureEnvironmentFixedObjects(){
+    void TeoCheckCollisionsLibrary::configureEnvironmentFixedObjects()
+    {
         yarp::os::Property fullConfig;
         fullConfig.fromConfigFile(m_fixedObjectsFileFullPath.c_str());
         m_numFixedObjects = fullConfig.check("numFixedObjects", yarp::os::Value(DEFAULT_NUM_COLLISION_OBJETCS)).asInt32();
@@ -25,26 +28,27 @@ namespace roboticslab
         for (int fixedObjectIndex = 0; fixedObjectIndex < m_numFixedObjects; fixedObjectIndex++)
         {
             std::string fixedObjectStr = "fixedObject_" + std::to_string(fixedObjectIndex);
-            yarp::os::Bottle & bFixedObject = fullConfig.findGroup(fixedObjectStr);
+            yarp::os::Bottle &bFixedObject = fullConfig.findGroup(fixedObjectStr);
             if (!bFixedObject.isNull())
             {
                 printf("We have the fixedObject: %d\n", fixedObjectIndex);
                 yarp::os::Bottle fixedObjectPosition = bFixedObject.findGroup("pos", "position (SI units)").tail();
                 m_positionFixedObjects.push_back(std::array<float, 3>{fixedObjectPosition.get(0).asFloat32(),
-                                                                        fixedObjectPosition.get(1).asFloat32(),
-                                                                        fixedObjectPosition.get(2).asFloat32()});
+                                                                      fixedObjectPosition.get(1).asFloat32(),
+                                                                      fixedObjectPosition.get(2).asFloat32()});
                 yarp::os::Bottle fixedObjectOrientation = bFixedObject.findGroup("rot", "orientaion (quaternion qx qy qz qw)").tail();
                 m_orientationFixedObjects.push_back(std::array<float, 4>{fixedObjectOrientation.get(0).asFloat32(),
-                                                                        fixedObjectOrientation.get(1).asFloat32(),
-                                                                        fixedObjectOrientation.get(2).asFloat32(),
-                                                                        fixedObjectOrientation.get(3).asFloat32()});
-                
+                                                                         fixedObjectOrientation.get(1).asFloat32(),
+                                                                         fixedObjectOrientation.get(2).asFloat32(),
+                                                                         fixedObjectOrientation.get(3).asFloat32()});
+
                 printf("position: %f %f %f\n", m_positionFixedObjects.back()[0], m_positionFixedObjects.back()[1], m_positionFixedObjects.back()[2]);
                 printf("orientation: %f %f %f %f\n", m_orientationFixedObjects.back()[0], m_orientationFixedObjects.back()[1], m_orientationFixedObjects.back()[2], m_orientationFixedObjects.back()[3]);
 
-                yarp::os::Bottle bCollisionObjectBoxShape = bFixedObject.findGroup("boxShape", "collision box shape (SI units)").tail();                                                        
+                yarp::os::Bottle bCollisionObjectBoxShape = bFixedObject.findGroup("boxShape", "collision box shape (SI units)").tail();
 
-                if(!bCollisionObjectBoxShape.isNull()){
+                if (!bCollisionObjectBoxShape.isNull())
+                {
                     printf("Fixed object is defined as a box of size: %f %f %f", bCollisionObjectBoxShape.get(0).asFloat32(), bCollisionObjectBoxShape.get(1).asFloat32(), bCollisionObjectBoxShape.get(2).asFloat32());
                     fcl::Transform3f tfTest;
                     CollisionGeometryPtr_t collisionGeometryAux{new fcl::Boxf{bCollisionObjectBoxShape.get(0).asFloat32(), bCollisionObjectBoxShape.get(1).asFloat32(), bCollisionObjectBoxShape.get(2).asFloat32()}};
@@ -52,28 +56,33 @@ namespace roboticslab
                     fcl::Quaternionf rotation(m_orientationFixedObjects.back()[3], m_orientationFixedObjects.back()[0], m_orientationFixedObjects.back()[1], m_orientationFixedObjects.back()[2]);
                     fcl::Vector3f translation(m_positionFixedObjects.back()[0], m_positionFixedObjects.back()[1], m_positionFixedObjects.back()[2]);
                     m_fixedCollisionObjects.back().setTransform(rotation, translation);
-                    m_boxShapesFixedObjects.push_back(std::array<float, 3>{bCollisionObjectBoxShape.get(0).asFloat32(), 
-                                                               bCollisionObjectBoxShape.get(1).asFloat32(), 
-                                                               bCollisionObjectBoxShape.get(2).asFloat32()}); 
+                    m_boxShapesFixedObjects.push_back(std::array<float, 3>{bCollisionObjectBoxShape.get(0).asFloat32(),
+                                                                           bCollisionObjectBoxShape.get(1).asFloat32(),
+                                                                           bCollisionObjectBoxShape.get(2).asFloat32()});
                 }
-                else{
+                else
+                {
                     printf("Error: The shape of fixedObject %d is defined wrong in file %s\n", fixedObjectIndex, m_fixedObjectsFileFullPath.c_str());
                 }
             }
-            else{
+            else
+            {
                 printf("Error: fixedObject %d not found in file %s\n", fixedObjectIndex, m_fixedObjectsFileFullPath.c_str());
             }
         }
     }
 
+
     bool TeoCheckCollisionsLibrary::collision()
     {
-        if (!selfCollision()){
+        if (!selfCollision())
+        {
             fcl::CollisionRequestf requestType;
             fcl::CollisionResultf collisionResult;
-            for (int link1 = 0; link1<m_collisionObjects.size(); link1++)
+            for (int link1 = 0; link1 < m_collisionObjects.size(); link1++)
             {
-                for(int fixedObject = 0; fixedObject<m_fixedCollisionObjects.size(); fixedObject++){
+                for (int fixedObject = 0; fixedObject < m_fixedCollisionObjects.size(); fixedObject++)
+                {
                     fcl::collide(&m_collisionObjects[link1], &m_fixedCollisionObjects[fixedObject], requestType, collisionResult);
                     if (collisionResult.isCollision())
                     {
@@ -85,16 +94,73 @@ namespace roboticslab
             printf("collision() not collide\n");
             return false;
         }
-        else{
+        else
+        {
             printf("collision() not collide\n");
             return false;
         }
     }
+    void TeoCheckCollisionsLibrary::setSuperquadrics(const std::vector<int> label_idx, const std::vector<std::array<float,11>> params)
+    {
+     m_superquadrics.clear();
+        for (int i = 0; i<label_idx.size(); i++){
+            SuperQuadric s;
+            s.label_idx = label_idx[i];
+            s.params = params[i];
+            m_superquadrics.push_back(s);
+        }
+    }
 
-/************************************************************************/
+    void TeoCheckCollisionsLibrary::getSuperquadrics(std::vector<int>& label_idx, std::vector<std::array<float,11>> &params){
+        label_idx.clear();
+        params.clear();
+        for(int i=0; i<m_superquadrics.size(); i++){
+            label_idx.push_back(m_superquadrics[i].label_idx);
+            params.push_back(m_superquadrics[i].params);
+        }
+    }
 
-/************************************************************************/
+    void TeoCheckCollisionsLibrary::updateEnvironmentCollisionObjects(){
+        for(int i=0; i<m_superquadrics.size(); i++){
+            if(m_superquadrics[i].params[3]<DEFAULT_E_LIMIT1){
+                if(m_superquadrics[i].params[4]<DEFAULT_E_LIMIT1){ //Box
+                    printf("Add box");
+                    fcl::Transform3f tfTest;
+                    //TODO: MODIFY POSITION AND ROTATION
+                    CollisionGeometryPtr_t collisionGeometryAux{new fcl::Boxf{m_superquadrics[i].params[0], m_superquadrics[i].params[1], m_superquadrics[i].params[2]}};
+                    m_environmentCollisionObjects.push_back(fcl::CollisionObjectf{collisionGeometryAux, tfTest});
+                }
+                if((m_superquadrics[i].params[4]>=DEFAULT_E_LIMIT1) && (m_superquadrics[i].params[4]<DEFAULT_E_LIMIT2)){ //Cylinder
+                    printf("Add cylinder");
+                    fcl::Transform3f tfTest;
+                    //TODO: MODIFY POSITION AND ROTATION
+                    CollisionGeometryPtr_t collisionGeometryAux{new fcl::Cylinderf{m_superquadrics[i].params[0], m_superquadrics[i].params[2]}};
+                    m_environmentCollisionObjects.push_back(fcl::CollisionObjectf{collisionGeometryAux, tfTest});
+                }
+                if((m_superquadrics[i].params[4]>=DEFAULT_E_LIMIT2)){ //Box
+                    printf("Add box");
+                    fcl::Transform3f tfTest;
+                    //TODO: MODIFY POSITION AND ROTATION
+                    CollisionGeometryPtr_t collisionGeometryAux{new fcl::Boxf{m_superquadrics[i].params[0], m_superquadrics[i].params[1], m_superquadrics[i].params[2]}};
+                    m_environmentCollisionObjects.push_back(fcl::CollisionObjectf{collisionGeometryAux, tfTest});
+                }
+            }
+            if(m_superquadrics[i].params[3]>=DEFAULT_E_LIMIT1){
+                printf("Add ellipsoid");
+                fcl::Transform3f tfTest;
+                 //TODO: MODIFY POSITION AND ROTATION
+                CollisionGeometryPtr_t collisionGeometryAux{new fcl::Ellipsoidf{m_superquadrics[i].params[0], m_superquadrics[i].params[1], m_superquadrics[i].params[2]}};
+                m_environmentCollisionObjects.push_back(fcl::CollisionObjectf{collisionGeometryAux, tfTest});
+            }
 
-/************************************************************************/
+        }
+    }
+
+
+    /************************************************************************/
+
+    /************************************************************************/
+
+    /************************************************************************/
 
 } // namespace roboticslab
--- libraries/TeoCheckCollisionsLibrary/TeoCheckCollisionsLibrary.hpp
+++ libraries/TeoCheckCollisionsLibrary/TeoCheckCollisionsLibrary.hpp
@@ -4,11 +4,19 @@
 #define __TEO_CHECK_COLLISIONS_LIBRARY_HPP__
 
 #include "TeoCheckSelfCollisionsLibrary.hpp"
+#define DEFAULT_E_LIMIT1 0.5
+#define DEFAULT_E_LIMIT2 1.2
+#define DEFAULT_E_LIMIT3 2.0
 
 using CollisionGeometryPtr_t = std::shared_ptr<fcl::CollisionGeometryf>;
 
 namespace roboticslab
 {
+
+struct SuperQuadric{
+    int label_idx;
+    std::array<float,11> params;
+};
 
 /**
  * @ingroup teoCheckCollisionsLibrary
@@ -26,8 +34,13 @@ public:
        boxShapesFixedObjects = m_boxShapesFixedObjects;
     }
     void getFixedObjectTransformations(std::vector<std::array<double,7>> & transformations);
+
     bool collision();
 
+    void setSuperquadrics(const std::vector<int> label_idx, const std::vector<std::array<float,11>> params);
+    void getSuperquadrics(std::vector<int>& label_idx, std::vector<std::array<float,11>> &params);
+
+    void updateEnvironmentCollisionObjects();
 
 protected:
     std::vector<fcl::CollisionObjectf> m_environmentCollisionObjects;
@@ -38,7 +51,7 @@ protected:
     std::vector<std::array<float,3>> m_positionFixedObjects;
     std::vector<std::array<float,4>> m_orientationFixedObjects;
     std::vector<std::array<float,3>> m_boxShapesFixedObjects;
-
+    std::vector<SuperQuadric> m_superquadrics;
 
 };
 
